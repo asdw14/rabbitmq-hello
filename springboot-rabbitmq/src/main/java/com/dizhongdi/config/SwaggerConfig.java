2d09c835751bdf83b625bc61724e75cd9fe4d5d7
dizhongdi <2755063993@qq.com>
2022-07-23 23:11:40 +0800

    springboot集成

---
+++ springboot-rabbitmq/src/main/java/com/dizhongdi/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.dizhongdi.config;
+
+import io.swagger.annotations.SwaggerDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * ClassName:SwaggerConfig
+ * Package:com.dizhongdi.config
+ * Description:
+ *
+ * @Date: 2021/9/14 4:37
+ * @Author:dizhongdi
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .build();
+    }
+    private ApiInfo webApiInfo(){
+        return new ApiInfoBuilder()
+                .title("rabbitmq 接口文档")
+                .description("本文档描述了 rabbitmq 微服务接口定义")
+                .version("1.0")
+                .build();
+    }
+}
