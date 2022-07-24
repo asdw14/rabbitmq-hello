9693492dc6212adde96fa0e30ca8a0be965a97a1
dizhongdi <2755063993@qq.com>
2022-07-24 23:56:45 +0800

    备份交换机

---
+++ springboot-rabbitmq/src/main/java/com/dizhongdi/consumer/WarningConsumer.java
@@ -0,0 +1,24 @@
+package com.dizhongdi.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * ClassName:WarningConsumer
+ * Package:com.dizhongdi.consumer
+ * Description:
+ *      报警消费者
+ * @Date: 2022/7/24 23:54
+ * @Author:dizhongdi
+ */
+@Component
+@Slf4j
+public class WarningConsumer {
+    public static final String WARNING_QUEUE_NAME = "warning.queue";
+    @RabbitListener(queues = WARNING_QUEUE_NAME)
+    public void receiveWarningMsg(Message message) {
+        String msg = new String(message.getBody());
+        log.error("报警发现不可路由消息：{}", msg);
+    } }
